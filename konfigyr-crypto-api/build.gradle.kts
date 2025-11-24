76a1d9360f081e561d06be870691e6a6a9d5864c
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2025-11-24 18:48:12 +0100

    Gradle and GH actions update

---
+++ konfigyr-crypto-api/build.gradle.kts
@@ -0,0 +1,7 @@
+description = "Core library of the Konfigyr Crypto library that defines an extensible API for working with crypto keys"
+
+dependencies {
+    compileOnly("org.springframework.boot:spring-boot-starter")
+
+	testImplementation("org.springframework.boot:spring-boot-starter")
+}
---
+++ konfigyr-crypto-jdbc/build.gradle.kts
@@ -0,0 +1,9 @@
+description = "Konfigyr Crypto library that uses Spring Data JDBC to store your encrypted key material"
+
+dependencies {
+    api(project(":konfigyr-crypto-api"))
+    compileOnly("org.springframework.boot:spring-boot-starter-jdbc")
+
+    testImplementation("org.hsqldb:hsqldb")
+	testImplementation("org.springframework.boot:spring-boot-starter-jdbc")
+}
